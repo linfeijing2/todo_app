a3914d50dbae28830bd89a5fdb528fed000727ec
LinfeiJing <ljing2@illinois.edu>
2022-03-10 18:00:12 -0600

    version with todolist model

--- app.py
+++ app.py
@@ -7,14 +7,24 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jlf:myPassword@localhost:5
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
+##childe
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
-    
+    todolist_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
+
+##parent table
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='todoparent', lazy=True)
+    
+
 # db.create_all()
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
